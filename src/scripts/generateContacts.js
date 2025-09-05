71b93d192226edb2646903322ddae7eccf5c23e2
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-09-05 22:26:39 +0300

    three tasks

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { faker } from '@faker-js/faker';
+
+const generateContacts = async (number) => {
+  const fakeContacts = faker.helpers.multiple(createFakeContact, {
+    count: 5,
+  });
+  // console.log(contacts);
+
+  const dbContacts = await readContacts();
+
+  console.log(dbContacts);
+
+  const allContacts = [...dbContacts, ...fakeContacts];
+
+  console.log(allContacts);
+
+  writeContacts(allContacts);
+};
 
 generateContacts(5);
